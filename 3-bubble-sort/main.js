6ab2a7813179ea22a3bcf897b180f4bff70cc1a4
Joed <joeddonaire@gmail.com>
2026-03-28 16:18:27 +0800

    Recalled how to create a Bublle sort function

--- 3-bubble-sort/main.js
+++ 3-bubble-sort/main.js
@@ -1,1 +1,27 @@
-console.log("hello world") ;
+let myarray = [4,2,7,9,1,3,6,5,8] ;
+console.log("This is the unsorted array") ;
+console.log(myarray) ;
+
+
+function BubbleSort(array) {
+
+    let temp = 0 ;
+    for(let i = 0 ; i < array.length ; i++) {
+        for(let j = i ; j < array.length  ; j++) {
+            if(array[i] > array[j]) {
+                temp = array[i] ;
+                array[i] = array[j] ;
+                array[j] = temp ; 
+            }
+        }
+    }
+    return array; 
+}
+
+let sortedArray = BubbleSort(myarray) ;
+
+
+
+console.log("----------")
+console.log("This is my sorted array") ;
+console.log(sortedArray) ;
