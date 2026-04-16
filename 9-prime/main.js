a1ce89c4096c79a7e975bd75fb492261c26349d9
Joed <joeddonaire@gmail.com>
2026-04-16 16:57:19 +0800

    Created a simple prime number function

---
+++ 9-prime/main.js
@@ -0,0 +1,20 @@
+let number = 5 ; 
+
+
+function isPrime(num) {
+
+    if ( num < 2 ) {
+        return false ;
+    } else {
+        for(let i = 2;i < num; i++) {
+            if(num % i === 0) {
+                return false ;
+            }
+        }
+    }
+
+
+    return true; 
+}
+
+console.log(isPrime(4)) ;
